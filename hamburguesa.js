76a0c8f1b44112e7143e1e5b6929eb532cb60929
Michelle Mullett <44821939+Michelle-M@users.noreply.github.com>
2019-04-05 00:39:19 -0300

    Initial commit

---
+++ hamburguesa.js
@@ -0,0 +1,7 @@
+var hambur = document.getElementById('hamburguesa');
+var menuMobile = document.getElementById('menu-mobile')
+
+hambur.onclick = function() {
+    menuMobile.classList.toggle('displayNone-2')
+}
+
